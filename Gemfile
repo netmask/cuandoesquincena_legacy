d5318186c7b1b903112e21daf97062adf80a9d54
Jonathan Graray <jonathan@devmask.net>
2015-02-16 12:16:04 -0700

    Cuando es quincena initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'business_time'
+gem 'icalendar', '~> 2.2.2'
+gem 'haml'
+gem 'activesupport'
+gem 'puma'
+gem 'color'
---
+++ app.rb
@@ -0,0 +1,51 @@
+require 'active_support'
+require 'sinatra'
+require 'haml'
+require 'business_time'
+
+helpers do
+  def class_by_days(days)
+    style = case days
+            when 11..15 then 'red'
+            when 7..11 then 'orange'
+            when 3..7 then 'yellow'
+            when 0..3 then 'green'
+            end
+
+    "<div class='#{style}'> #{days} </div>"
+  end
+end
+
+class Quincena
+  # 0 Is Sunday
+  # 6 Is Sat
+  NO_WORK_DAYS = {6 => 1, 0 => 2}
+
+  def left_days
+    (next_pay_date - Date.today).to_i
+  end
+
+  def is_today?
+    next_pay_date == Date.today
+  end
+
+  def next_pay_date
+    date_time = DateTime.now
+    canonical = Date.civil(date_time.year, date_time.month, next_canonical_day)
+
+    if NO_WORK_DAYS.keys.include?(canonical.wday)
+      canonical = Date.civil(date_time.year, date_time.month, next_canonical_day - NO_WORK_DAYS[canonical.wday])
+    end
+
+    canonical
+  end
+
+  def next_canonical_day
+    Date.today.day > 15 ? -1 : 15
+  end
+end
+
+get '/' do
+  @quincena = Quincena.new
+  haml :index
+end
